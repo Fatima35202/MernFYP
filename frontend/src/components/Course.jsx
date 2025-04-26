39ee4c7c78def7d8133a9cd8c06aba3ec865bedc
Fatima35202 <162595090+Fatima35202@users.noreply.github.com>
2025-04-26 04:55:21 -0700

    Update Course.jsx

--- frontend/src/components/Course.jsx
+++ frontend/src/components/Course.jsx
@@ -2,8 +2,10 @@ import React, { useEffect, useState } from "react";
 import Cards from "./Cards";
 import axios from "axios";
 import { Link } from "react-router-dom";
+
 function Course() {
   const [magazine, setMagazine] = useState([]);
+
   useEffect(() => {
     const getMagazine = async () => {
       try {
@@ -16,13 +18,14 @@ function Course() {
     };
     getMagazine();
   }, []);
+
   return (
     <>
-      <div className=" max-w-screen-2xl container mx-auto md:px-20 px-4">
-        <div className="mt-28 items-center justify-center text-center">
-          <h1 className="text-2xl  md:text-4xl">
+      <div className="max-w-screen-2xl container mx-auto md:px-20 px-4">
+        <div className="mt-28 flex flex-col items-center justify-center text-center">
+          <h1 className="text-2xl md:text-4xl">
             We're delighted to have you{" "}
-            <span className="text-orange-500"> Here! :)</span>
+            <span className="text-orange-500">Here! :)</span>
           </h1>
           <p className="mt-12">
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro,
@@ -39,7 +42,8 @@ function Course() {
             </button>
           </Link>
         </div>
-        <div className="mt-12 grid grid-cols-1 md:grid-cols-4">
+
+        <div className="mt-12 grid grid-cols-1 md:grid-cols-4 gap-6">
           {magazine.map((item) => (
             <Cards key={item.id} item={item} />
           ))}
