c4d74568bd591c8d48088d891a012e959039dc24
Fatima35202 <162595090+Fatima35202@users.noreply.github.com>
2025-04-26 04:38:01 -0700

    Update Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import axios from "axios";
 import toast from "react-hot-toast";
+
 function Login() {
   const {
     register,
@@ -15,32 +16,31 @@ function Login() {
       email: data.email,
       password: data.password,
     };
-    await axios.post('https://mernfyp-production.up.railway.app/user/login', userInfo);
-      .then((res) => {
-        console.log(res.data);
-        if (res.data) {
-          toast.success("Loggedin Successfully");
-          document.getElementById("my_modal_3").close();
-          setTimeout(() => {
-            window.location.reload();
-            localStorage.setItem("Users", JSON.stringify(res.data.user));
-          }, 1000);
-        }
-      })
-      .catch((err) => {
-        if (err.response) {
-          console.log(err);
-          toast.error("Error: " + err.response.data.message);
-          setTimeout(() => {}, 2000);
-        }
-      });
+
+    try {
+      const res = await axios.post('https://mernfyp-production.up.railway.app/user/login', userInfo);
+      console.log(res.data);
+      if (res.data) {
+        toast.success("Logged in Successfully");
+        document.getElementById("my_modal_3").close();
+        setTimeout(() => {
+          localStorage.setItem("Users", JSON.stringify(res.data.user));
+          window.location.reload();
+        }, 1000);
+      }
+    } catch (err) {
+      if (err.response) {
+        console.log(err);
+        toast.error("Error: " + err.response.data.message);
+      }
+    }
   };
+
   return (
     <div>
       <dialog id="my_modal_3" className="modal">
         <div className="modal-box">
-          <form onSubmit={handleSubmit(onSubmit)} method="dialog">
-            {/* if there is a button in form, it will close the modal */}
+          <form onSubmit={handleSubmit(onSubmit)}>
             <Link
               to="/"
               className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
@@ -50,7 +50,8 @@ function Login() {
             </Link>
 
             <h3 className="font-bold text-lg">Login</h3>
-            {/* Email */}
+
+            {/* Email Field */}
             <div className="mt-4 space-y-2">
               <span>Email</span>
               <br />
@@ -67,7 +68,8 @@ function Login() {
                 </span>
               )}
             </div>
-            {/* password */}
+
+            {/* Password Field */}
             <div className="mt-4 space-y-2">
               <span>Password</span>
               <br />
@@ -85,9 +87,12 @@ function Login() {
               )}
             </div>
 
-            {/* Button */}
+            {/* Buttons */}
             <div className="flex justify-around mt-6">
-              <button className="bg-orange-500 text-white rounded-md px-3 py-1 hover:bg-orange-700 duration-200">
+              <button
+                type="submit"
+                className="bg-orange-500 text-white rounded-md px-3 py-1 hover:bg-orange-700 duration-200"
+              >
                 Login
               </button>
               <p>
@@ -97,7 +102,7 @@ function Login() {
                   className="underline text-blue-500 cursor-pointer"
                 >
                   Signup
-                </Link>{" "}
+                </Link>
               </p>
             </div>
           </form>
